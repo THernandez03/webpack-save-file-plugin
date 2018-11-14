2012709897567c78cac8137a8c62b4f7f92b757b
Tomás Hernández <tomas.hernandez03@gmail.com>
2018-11-14 00:07:30 -0300

    Make it work

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+export default class DiskPlugin {
+  constructor({ debugAssets, output = '.', files = [] } = {}) {
+    this.options = { output, files, debugAssets };
+  }
+
+  apply(compiler) {
+    const { debugAssets, output: baseOutput, files } = this.options;
+    const assetsToFind = Array.isArray(files) ? files : [files];
+    const assetsToUse = new Map();
+
+    compiler.hooks.afterEmit.tapAsync(
+      'DiskPlugin',
+      // eslint-disable-next-line promise/prefer-await-to-callbacks
+      async (curCompiler, callback) => {
+        const { assets } = curCompiler;
+        const webpackAssets = Object.keys(assets);
+
+        if (debugAssets) console.log(webpackAssets);
+
+        assetsToFind.forEach(({ asset, output }) => {
+          const foundAsset = webpackAssets.find(
+            webpackAsset =>
+              Object.getPrototypeOf(asset).constructor === String
+                ? asset === webpackAsset
+                : asset.test(webpackAsset),
+          );
+
+          if (!foundAsset) return null;
+
+          const pathToUse = path.resolve(output || baseOutput, foundAsset);
+
+          if (assetsToUse.has(pathToUse)) return null;
+
+          return assetsToUse.set(pathToUse, assets[foundAsset]);
+        });
+
+        await Promise.all(
+          [...assetsToUse.entries()].map(async ([file, asset]) => {
+            const directory = path.dirname(file);
+            const { F_OK, W_OK } = fs.constants;
+
+            await promisify(fs.access)(directory, F_OK | W_OK).catch(() =>
+              promisify(fs.mkdir)(directory),
+            );
+            await promisify(fs.writeFile)(file, asset.source()).catch(() =>
+              // eslint-disable-next-line promise/no-callback-in-promise, promise/prefer-await-to-callbacks
+              callback(new Error(`Error writing ${file}`)),
+            );
+          }),
+        );
+
+        // eslint-disable-next-line promise/prefer-await-to-callbacks
+        return callback();
+      },
+    );
+  }
+}
